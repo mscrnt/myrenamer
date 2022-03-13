ff36ef61913bcd9b6e0dce8dddedc0d556a4811e
Kenneth <trunksGT243@gmail.com>
2022-03-13 08:36:47 +0000

    Ajax

--- app/routes.py
+++ app/routes.py
@@ -94,7 +94,7 @@ def before_request():
     if current_user.is_authenticated:
         current_user.last_seen = datetime.utcnow()
         db.session.commit()
-    g.local = str(get_locale())
+    g.locale = str(get_locale())
 
 @app.route('/edit_profile', methods=['GET', 'POST'])
 @login_required
--- app/translate.py
+++ app/translate.py
@@ -2,6 +2,7 @@ import json
 import requests
 from flask_babel import _
 from app import app
+import sys
 
 
 def translate(text, source_language, dest_language):
