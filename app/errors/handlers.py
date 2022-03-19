9e9f82a8d67a35435c4a34e24324077f0a2cfae5
Kenneth <trunksGT243@gmail.com>
2022-03-19 01:26:50 +0000

    Correcting error in handlers.py

--- app/errors/handlers.py
+++ app/errors/handlers.py
@@ -4,7 +4,7 @@ from app.errors import bp
 
 @bp.app_errorhandler(404)
 def not_found_error(error):
-    return render_template('errors/404.html', 404)
+    return render_template('errors/404.html'), 404
 
 @bp.app_errorhandler(500)
 def internal_error(error):
